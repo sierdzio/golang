2cd17add51ca3d8ff733c3c4ea4751da089bc1ec
Tomasz Siekierda (sierdzio) <tsiekierda@milosolutions.com>
2018-06-28 21:48:26 +0200

    Just testing some basic basics.

---
+++ clitest1/clitest1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "log"
+  "os"
+
+  "github.com/urfave/cli"
+)
+
+func main() {
+  app := cli.NewApp()
+
+  app.Flags = []cli.Flag {
+    cli.StringFlag{
+      Name: "lang, l",
+      Value: "english",
+      Usage: "language for the greeting",
+    },
+  }
+
+  err := app.Run(os.Args)
+  if err != nil {
+    log.Fatal(err)
+  }
+}
---
+++ hello/hello.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界. Whoever's idea it was to use TABS in Go language? Horrible.")
+}
